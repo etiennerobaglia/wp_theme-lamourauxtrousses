1fbdb2ce54e8c3c5d1caf38037c3e6a92150742d
Etienne Robaglia <etiennerobaglia@MBP-de-Etienne.lan>
2020-03-03 10:21:33 +0700

    header sidebar laat vh vw

--- sidebar.php
+++ sidebar.php
@@ -13,5 +13,12 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 ?>
 
 <aside id="secondary" class="widget-area">
+<div class="site-branding">
+			
+				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+					<img id="img-logo" alt="L'Amour Aux Trousses" src="/wordpress/wp-content/themes/themamourotrousse/img/laat-logo_beige.svg">
+				</a>
+</div><!-- .site-branding -->
+
 	<?php dynamic_sidebar( 'sidebar-1' ); ?>
 </aside><!-- #secondary -->
